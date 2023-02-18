675c67b2e2b42e63bfa33a1ec015d46b351e7a7f
Oluchi Alaribe <bamarachi6@gmail.com>
2023-02-18 16:45:47 +0100

    Adding files

---
+++ README.md
@@ -0,0 +1,38 @@
+<!-- Solution to Question Nos 1 and 2 -->
+
+Arithmetic Operators: + - * / % ** ++ --
+<!-- Examples -->
+let num1 = 2;
+let num2 = 4;
+console.log(num1 + num2);
+console.log(num1 * num2);
+
+Assignment Operators: = += -= *= /=
+<!-- Examples -->
+let num1 = 2;
+let num1 += 20;
+
+Comparison Operators: > < <= >= === == != !==
+<!-- Examples -->
+let num1 = 5;
+let num2 = 2;
+console.log(num1 == 5);
+console.log(num2 === '2');
+
+Logical Operators: && || !
+<!-- Examples -->
+const a = true; 
+const b = false;
+console.log(a && a); 
+console.log(a || b);
+
+Bitwise Operators: & | ^ ~ << >> >>>
+<!-- Examples -->
+let num1 = 12;
+let num2 = 25;
+console.log(num1 & num2);
+console.log(num1 | num2);
+
+<!-- Answer to Question No 4 -->
+<!-- When the program is executed, it will log the values of the variable, i to the console three times.
+The loop starts with i = 1 and increments i by 7 in each iteration, so the loop will execute three times with the values of i being 1, 8, and 15. Once i reaches 22, the condition i < 20 becomes false, and the loop terminates. -->
---
+++ index.js
@@ -0,0 +1,34 @@
+// Solution to Quetion No 3 
+let Science = "Physics, Chemistry, Biology, Technical Drawing";
+let Arts = "Government, Economics, Literature, History";
+let socialScience = "Accounting, Commerce, Marketing, Geography";
+let General = "English, Mathematics";
+let classGroup = prompt("Enter your class group: ");
+
+if (classGroup === "Science") {
+  console.log("Here are the Science subjects: " + Science + "," + " " + General + ".");
+} else if(classGroup === "Arts") {
+  console.log("Here are the Arts subjects: " + Arts + "," + " " + General + ".");
+} else if(classGroup === "Social Science") {
+  console.log("Here are the Social Science subjects: " + socialScience + "," + " " + General + ".");
+} else {
+  console.log("Here are the general subjects: " + General + ".");
+}
+
+// Answer to Question No 4
+/* When the program is executed, it will log the values of the variable, i to the console three times.
+The loop starts with i = 1 and increments i by 7 in each iteration, so the loop will execute three times with the values of i being 1, 8, and 15. Once i reaches 22, the condition i < 20 becomes false, and the loop terminates. */
+
+// Solution to Question No 5
+function nearestPowerOfTwo(num) {
+  let pwr = 1;
+  while (pwr < num) {
+    pwr *= 2;
+  }
+  return pwr;
+}
+
+let num = 40;
+let pwr = nearestPowerOfTwo(num);
+
+console.log("The number " + pwr + " " + "is the power of 2 nearest to" + " " + num)
